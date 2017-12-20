0f5f5ff82dc787e649e4451091379a7039a8558a
John Leidegren <john@tessin.se>
2017-12-20 14:05:01 +0100

    First commit

---
+++ src/StringUtils.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace Tessin
+{
+    static class StringUtils
+    {
+        public static string NormalizeLineEndings(string s)
+        {
+            // normalize line endings to use *nix style only
+            var writer = new StringWriter { NewLine = "\n" };
+            using (var reader = new StringReader(s.Trim()))
+            {
+                for (; ; )
+                {
+                    var line = reader.ReadLine();
+                    if (line == null)
+                    {
+                        break;
+                    }
+                    writer.WriteLine(line);
+                }
+            }
+            return writer.ToString();
+        }
+    }
+}
---
+++ src/tick.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+
+FUNC_HOST=tessin-raspi.azurewebsites.net
+FUNC_CODE=wt1nf81dnShiaZ2DznD1iR44a64VikIdB%2FyWPzRYpzlByQwNg2DVpQ%3D%3D
+FUNC_TICK="https://$FUNC_HOST/api/pi/hosts/$(hostname)/tick?code=$FUNC_CODE"
+
+curl $FUNC_TICK -o tock.sh
+chmod u+x tock.sh
+./tock.sh
